5181c9aa7cd8788d5f1fc48cf17a7a327cb10c9a
dineshabe <dineshabe@gmail.com>
2025-06-05 09:42:31 -0600

    some refactor

--- Sources/FileManager.swift
+++ Sources/FileManager.swift
@@ -16,3 +16,49 @@ struct FileManager {
        }
     }
 }
+
+struct StringCatalogManager {
+    let fileManager = FileManager()
+    
+    func fetch(fileName: String, language: String) -> [String: String]{
+        do {
+            if let catalog = try fileManager.readFile(fileName) {
+                return parse(catalog, language: language)
+            }
+        } catch {
+            if let fileError = error as? FileError {
+                switch fileError {
+                case .badFileName:
+                    print("Bad file name")
+                case .cannotRead(let message):
+                    print("Unable to read file \(message)")
+                }
+            } else {
+                print("Unknown error")
+            }
+        }
+        return [:]
+    }
+    
+    func parse(_ catalog: StringCatalog, language: String) -> [String: String] {
+        var result = [String: String]()
+        
+        for item in catalog.strings {
+            if let localizations = item.value.localizations, let translation = localizations[language]?.stringUnit.value {
+                result[item.key] = translation
+            } else {
+                result[item.key] = item.key
+            }
+        }
+        return result
+    }
+}
+
+struct OutputStream {
+    func write(_ outputValue: [String: String]) {
+        let sortedResult = outputValue.sorted { $0.key < $1.key }
+        for (key, value) in sortedResult {
+            print("\(key) = \(value)")
+        }
+    }
+}
--- Sources/xLocalizr.swift
+++ Sources/xLocalizr.swift
@@ -3,57 +3,23 @@ import Foundation
 @main
 struct XLocalizrTool {
     
-    static let fileManager = FileManager()
     
   static func main() {
       
+      let stringCatalog = StringCatalogManager()
+      let output = OutputStream()
+      
+      //Argument manager
       let arguments = CommandLine.arguments
       guard arguments.count > 1 else {
           print("Usage: xLocalizr <filename> <language>(optional defaults to en)")
           exit(1)
       }
-
       let fileName = arguments[1]
       let language = arguments.count > 2 ? arguments[2] : "en"
       print("Open file \(fileName) and fetch translations for \(language)")
-      
-      do {
-          if let catalog = try fileManager.readFile(fileName) {
-              let parsedResult = parse(catalog, language: language)
-              outputResult(parsedResult)
-          }
-      } catch {
-          if let fileError = error as? FileError {
-              switch fileError {
-              case .badFileName:
-                  print("Bad file name")
-              case .cannotRead(let message):
-                  print("Unable to read file \(message)")
-              }
-          } else {
-              print("Unknown error")
-          }
-      }
+    
+      let catalog = stringCatalog.fetch(fileName: fileName, language: language)
+      output.write(catalog)
   }
-    
-    static func parse(_ catalog: StringCatalog, language: String) -> [String: String] {
-        var result = [String: String]()
-        
-        for item in catalog.strings {
-            if let localizations = item.value.localizations, let translation = localizations[language]?.stringUnit.value {
-                result[item.key] = translation
-            } else {
-                result[item.key] = item.key
-            }
-        }
-        return result
-    }
-    
-    static func outputResult(_ dict: [String: String]) {
-        let sortedKey = dict.sorted { $0.key < $1.key }
-
-        for (key, value) in sortedKey {
-            print("\(key): \(value)")
-        }
-    }
 }
